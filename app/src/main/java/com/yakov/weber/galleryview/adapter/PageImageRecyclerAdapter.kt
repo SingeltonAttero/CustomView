ec2142fccb1586b7837ebb5eb016b3d1e06d457b
yakov <cen7ur1on007@gmail.com>
2018-12-04 00:33:35 +0200

    fix check position gallery view

--- app/src/main/java/com/yakov/weber/galleryview/adapter/PageImageRecyclerAdapter.kt
+++ app/src/main/java/com/yakov/weber/galleryview/adapter/PageImageRecyclerAdapter.kt
@@ -14,52 +14,37 @@ import kotlinx.android.synthetic.main.item_page_adapter.view.*
  * @author YWeber
  * project GalleryView */
 
-class PageImageRecyclerAdapter (private val listUrlImage: List<String>,val clickItem:(Int,String) -> Unit) :
+class PageImageRecyclerAdapter(private val listUrlImage: List<String>, val clickItem: (Int, String) -> Unit) :
     RecyclerView.Adapter<PageImageRecyclerAdapter.PageImageViewHolder>() {
 
-    private val allView:MutableList<View> = arrayListOf()
-    private val selectedView:MutableList<View> = arrayListOf()
-    private val selectPositionView:SparseBooleanArray = SparseBooleanArray()
+    private var selectPos:Int = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PageImageViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_page_adapter,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_page_adapter, parent, false)
         return PageImageViewHolder(view)
     }
+
+
 
     override fun getItemCount(): Int = listUrlImage.size
 
     override fun onBindViewHolder(holder: PageImageViewHolder, position: Int) {
         holder.bind(listUrlImage[position])
-        holder.itemView.isSelected = selectPositionView.get(position,false)
-        allView.add(holder.itemView)
-    }
-
-    fun setCurrentSelectedView(position:Int){
-        selectedView.forEachIndexed { index, _ ->
-            selectedView.removeAt(index).isSelected = false
+        holder.itemView.isSelected = selectPos == position
+        holder.itemView.setOnClickListener {
+            notifyItemChanged(selectPos)
+            selectPos = position
+            notifyItemChanged(position)
+            clickItem(position,listUrlImage[position])
         }
-        selectedView.add(allView[position])
-        allView[position].isSelected = true
+
     }
 
-    inner class PageImageViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
-        fun bind(urlString:String){
+    inner class PageImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(urlString: String) {
             Glide.with(itemView.page_image_view.context)
-                .load(listUrlImage[adapterPosition])
+                .load(urlString)
                 .into(itemView.page_image_view)
-            itemView.setOnClickListener {
-               /* selectedView.forEachIndexed { index, _ ->
-                    selectedView.removeAt(index).isSelected = false
-                }*/
-                if (selectPositionView.get(adapterPosition,false)){
-                    selectPositionView.delete(adapterPosition)
-                    itemView.isSelected = false
-                }else{
-                    selectPositionView.put(adapterPosition,true)
-                    itemView.isSelected = true
-                }
-                clickItem(adapterPosition,urlString)
-            }
         }
 
     }
